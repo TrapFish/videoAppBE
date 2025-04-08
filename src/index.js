a05bbf6b6559b474172caaaa7673a216a8d1639c
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-04-08 21:55:16 +0530

    Changes for the cors, urlEncode, json, file, video, cookies

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: '../src/env'
 });
-connectDB();
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}).catch(e =>{
+    console.log("Error connecting to MongoDB:", e.message);
+
+})
 
 // import mongoose from "mongoose";
 // import {DB_NAME} from "./constants.js";
