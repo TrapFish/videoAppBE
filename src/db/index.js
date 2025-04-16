ba3c60991ff854687b29880ed074e8b8f2cc49c2
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-04-16 12:51:34 +0530

    changes for the env config

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 import {DB_NAME} from "../constants.js";
 
-
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
         console.log("MongoDB connected successfully:", connectionInstance.connection.host, process.env.MONGO_URI, DB_NAME);
     } catch (error) {
-        console.error("Error connecting to MongoDB:", error.message);
+        console.error("Error connecting to MongoDB sdfsdkjhjkh:", error.message);
         process.exit(1); // Exit the process with failure
     }
 }
